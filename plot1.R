dba413047f66977d60cd27eed87959b19f14aae3
Doris Smith <webgeekstress@gmail.com>
2014-09-05 13:50:52 +0300

    Changed the part of the script that loads the data

--- plot1.R
+++ plot1.R
@@ -3,29 +3,18 @@
 # Based upon Electric Power Consumption Data provided by
 # UC Irvine Machine Learning Repository 
 # https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
-# Unzip the above file into household_power_consumption.txt 
 
-# "Subsetting" the data before loading it all in
-# Manual inspection of the text file reveals that the data for 2/1/2007  
-# begins at row 66638 and the data for 2/3/2007 begins at row 69518
-
-firstRow  <- 66638 
-lastRow <- 69518 
-numRows  <- lastRow-firstRow  
-
-# Loading and formatting the data
-
+# Loading, subsetting, and formatting the data
 colNames <- c("Date", "Time", "Global Active Power", "Global Reactive Power", "Voltage", "Global Intensity", "Sub-metering 1", "Sub-metering 2", "Sub-metering 3")
+#colClasses <- c("Date", "character", "numeric","numeric","numeric","numeric","numeric","numeric","numeric" )
 dat <- read.table("household_power_consumption.txt", 
                   sep=";", 
-                  skip=66637, 
-                  nrow=numRows, 
                   col.names=colNames, 
-                  na.strings="?")
+                  na.strings="?",
+                  header=TRUE,
+                  stringsAsFactors=FALSE)
 
-# head(dat)  # Just checking that I'm starting in the right place
-# tail(dat)  # And that I'm ending in the right place
-
+dat<- subset(dat, (dat$Date == "1/2/2007" | dat$Date== "2/2/2007")) 
 dat$DateTime <- strptime(paste(dat$Date,dat$Time), "%d/%m/%Y %H:%M:%S")
 
 # Plotting the histogram and saving as a png file
--- plot2.R
+++ plot2.R
@@ -4,23 +4,15 @@
 # UC Irvine Machine Learning Repository 
 # https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
 
-# "Subsetting" the data before loading it all in
-# Manual inspection of the text file reveals that the data for 2/1/2007  
-# begins at row 66638 and the data for 2/3/2007 begins at row 69518
-
-firstRow  <- 66638 
-lastRow <- 69518 
-numRows  <- lastRow-firstRow  
-
-# Loading and formatting the data
-
+# Loading, subsetting, and formatting the data
 colNames <- c("Date", "Time", "Global Active Power", "Global Reactive Power", "Voltage", "Global Intensity", "Sub-metering 1", "Sub-metering 2", "Sub-metering 3")
 dat <- read.table("household_power_consumption.txt", 
                   sep=";", 
-                  skip=66637, 
-                  nrow=numRows, 
                   col.names=colNames, 
                   na.strings="?")
+
+dat <- dat[dat$Date == "1/2/2007"|dat$Date == "2/2/2007", ]
+
 dat$DateTime <- strptime(paste(dat$Date,dat$Time), "%d/%m/%Y %H:%M:%S")
 
 # Plotting the line chart and saving as a png file
